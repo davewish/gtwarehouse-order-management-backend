b6996893247788d22d97151d657d0ba936538da1
dawit tekle <dvwish9214@gmail.com>
2026-01-22 19:01:17 +0100

    feat:add core project structure for warehouse inventory and order management

---
+++ src/main/java/com/gt/warehouse/GtWarehouseManagementApplication.java
@@ -0,0 +1,13 @@
+package com.gt.warehouse;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GtWarehouseManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GtWarehouseManagementApplication.class, args);
+	}
+
+}
